ae8326a83c12eb2a24edcc1c90e2841c94d9a5d6
shaguftanaaz <shagufta680@gmail.com>
2017-08-15 00:49:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-    res.send('hi u r mad 1.');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req, res){
