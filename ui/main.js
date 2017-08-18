4ccfda0a3eb4a91ef6feebfd6be0d34c28f46519
shaguftanaaz <shagufta680@gmail.com>
2017-08-19 02:11:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 var img = document.getElementById('madi');
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+   // var interval = setInterval(moveRight, 100);
     img.style.marginLeft = '100px';  
 };
